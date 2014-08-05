6869bd51ee6198f5f9fa874f36557161b2c043ea
liamjbennett <lbennett@opentable.com>
2014-08-05 14:24:03 +0100

    fixing install_dir and adding symlink

--- manifests/broker/config.pp
+++ manifests/broker/config.pp
@@ -2,23 +2,28 @@
 #
 # This private class is called from kafka::broker to manage the configuration
 #
-class kafka::broker::config {
+class kafka::broker::config(
+  $install_dir = $kafka::broker::install_dir
+) {
 
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  $install_dir = "/usr/local/kafka-${kafka::broker::scala_version}-${kafka::broker::version}"
-
   $server_config = merge($kafka::params::broker_config_defaults, $kafka::broker::config)
 
-  file { '/usr/local/kafka/config/server.properties':
+  file { "${install_dir}/config/server.properties":
     owner   => 'kafka',
     group   => 'kafka',
     mode    => '0644',
     alias   => 'kafka-cfg',
     require => [ Exec['untar-kafka'], File['/usr/local/kafka'] ],
     content => template('kafka/server.properties.erb')
+  }
+  
+  file { "/opt/kafka":
+    ensure => link,
+    target => $install_dir
   }
 
   file { '/var/log/kafka':
