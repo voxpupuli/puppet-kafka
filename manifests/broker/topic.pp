4132e50a9dcbb404c24662fe8147e63f4c549d6e
liamjbennett <lbennett@opentable.com>
2014-08-06 11:45:29 +0100

    adding support for adding topics

---
+++ manifests/broker/topic.pp
@@ -0,0 +1,14 @@
+class kafka::broker::topic(
+  $ensure = '',
+  $zookeeper = '',
+  $replication_factor = 1,
+  $partions = 1
+) {
+  
+  if $ensure == 'present' {
+    exec { "create topic ${name}":
+      command => "${install_dir}/bin/kafka-topics.sh --create --zookeeper ${zookeeper} --replication-factor ${replication_factor} --partitions ${partitions} --topic ${name}",
+      unless  => "/bin/bash -c \"if [[ \\\"`${install_dir}/bin/kafka-topics.sh --list --zookeeper ${zookeeper} ${name}`\\\" == *${name}* ]]; then exit 0; else exit 1; fi\""
+    }  
+  }
+}
