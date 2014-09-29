82ad9ceedb7d3af7b40f8a6473aa268f22d965de
liamjbennett <lbennett@opentable.com>
2014-09-29 15:40:27 +0100

    bring kafka into the module standard

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,26 +1,36 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
+
 hosts.each do |host|
-  # Install Puppet
-  install_puppet
+  
+  
+	version = ENV['PUPPET_GEM_VERSION']
+	install_puppet(:version => version)
 end
 
-RSpec.configure do |c|
-  # Project root
+Spec.configure do |c|
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
-  # Readable test descriptions
-  c.formatter = :documentation
+	c.formatter = :documentation
 
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'kafka')
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppetlabs-java'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'deric-zookeeper'), { :acceptable_exit_codes => [0,1] }
-    end
+	c.before :suite do
+		#The gets around a bug where windows can't validate the cert when using https
+		forge_repo = '--module_repository=http://forge.puppetlabs.com'
+
+		hosts.each do |host|
+			c.host = host
+      
+			
+
+			path = (File.expand_path(File.dirname(__FILE__)+'/../')).split('/')
+			name = path[path.length-1].split('-')[1]
+
+			copy_module_to(host, :source => proj_root, :module_name => name)
+
+      
+			on host, puppet('module','install', forge_repo, "puppetlabs-stdlib"), { :acceptable_exit_codes => [0,1] }
+	    
+	  end
   end
 end
