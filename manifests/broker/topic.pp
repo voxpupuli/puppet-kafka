86614f18b228ce7abfeb18f22517583f19d01bbf
Vuokko Vuorinnen <VuokkoVuorinnen@users.noreply.github.com>
2015-06-25 10:51:18 +0200

    Update topic.pp
    The unless statement matched too much topics for example, if you had a 'foo' and 'foobar' topic, deleted the 'foo' topic and tried to recreate it, puppet wouldnt do it because '*foo*' still matches 'foobar'

--- manifests/broker/topic.pp
+++ manifests/broker/topic.pp
@@ -17,7 +17,7 @@ define kafka::broker::topic(
   if $ensure == 'present' {
     exec { "create topic ${name}":
       command => "/opt/kafka/bin/kafka-topics.sh --create --zookeeper '${zookeeper}' --replication-factor ${replication_factor} --partitions ${partitions} --topic ${name}",
-      unless  => "/bin/bash -c \"if [[ \\\"`/opt/kafka/bin/kafka-topics.sh --list --zookeeper '${zookeeper}' ${name}`\\\" == *${name}* ]]; then exit 0; else exit 1; fi\""
+      unless  => "/opt/kafka/bin/kafka-topics.sh --list --zookeeper ${zookeeper} | grep '^${name}\$'",
     }
   }
 }
