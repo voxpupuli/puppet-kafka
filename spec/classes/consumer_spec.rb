b2d36166178c1578bf74c4e565916a70e6ea5dbe
Dennis Hoppe <github@debian-solutions.de>
2016-03-07 16:51:57 +0100

    Extend RSpec tests

---
+++ spec/classes/consumer_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'kafka::consumer', :type => :class do
+  let :facts do
+    {
+      :osfamily               => 'Debian',
+      :operatingsystem        => 'Ubuntu',
+      :operatingsystemrelease => '14.04',
+      :lsbdistcodename        => 'trusty',
+      :architecture           => 'amd64',
+    }
+  end
+
+  it { is_expected.to contain_class('kafka::consumer::install').that_comes_before('Class[kafka::consumer::service]') }
+  it { is_expected.to contain_class('kafka::consumer::service').that_comes_before('Class[kafka::consumer]') }
+  it { is_expected.to contain_class('kafka::consumer') }
+
+  context 'on Debian' do
+    describe 'kafka::consumer::install' do
+      context 'defaults' do
+        it { is_expected.to contain_class('kafka') }
+      end
+    end
+
+    describe 'kafka::consumer::service' do
+      context 'defaults' do
+        it { is_expected.to contain_file('/etc/init.d/kafka-consumer') }
+
+        it { is_expected.to contain_service('kafka-consumer') }
+      end
+    end
+  end
+end
---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe 'kafka', :type => :class do
+  let :facts do
+    {
+      :osfamily               => 'Debian',
+      :operatingsystem        => 'Ubuntu',
+      :operatingsystemrelease => '14.04',
+      :lsbdistcodename        => 'trusty',
+      :architecture           => 'amd64',
+    }
+  end
+
+  it { is_expected.to contain_class('kafka::params') }
+
+  context 'on Debian' do
+    describe 'kafka' do
+      context 'defaults' do
+        it { is_expected.to contain_package('wget') }
+
+        it { is_expected.to contain_group('kafka') }
+        it { is_expected.to contain_user('kafka') }
+
+        it { is_expected.to contain_file('/var/lib/kafka') }
+        it { is_expected.to contain_file('/opt/kafka-2.10-0.8.2.1') }
+        it { is_expected.to contain_file('/opt/kafka') }
+        it { is_expected.to contain_file('/opt/kafka/config') }
+        it { is_expected.to contain_file('/var/log/kafka') }
+
+        it { is_expected.to contain_exec('download-kafka-package') }
+        it { is_expected.to contain_exec('untar-kafka-package') }
+      end
+    end
+  end
+end
