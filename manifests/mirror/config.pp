5a67c7da2c28eba6bc8142ba431dd4a88f1c4f44
Dennis Hoppe <github@debian-solutions.de>
2016-03-14 16:37:07 +0100

    Check for required parameters

--- manifests/mirror/config.pp
+++ manifests/mirror/config.pp
@@ -19,6 +19,16 @@ class kafka::mirror::config(
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
+  if $consumer_config['group.id'] == '' {
+    fail('[Consumer] You need to specify a value for group.id')
+  }
+  if $consumer_config['zookeeper.connect'] == '' {
+    fail('[Consumer] You need to specify a value for zookeeper.connect')
+  }
+  if $producer_config['metadata.broker.list'] == '' {
+    fail('[Producer] You need to specify a value for metadata.broker.list')
+  }
+
   ::kafka::consumer::config { 'consumer-1':
     config          => $consumer_config,
     config_defaults => $consumer_config_defaults,
