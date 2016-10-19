8b6d9be1777d12fa7e3185b59a409dd043f99295
David Ellam <david.ellam@williamhill.com>
2016-10-19 17:13:17 +0100

    support custom port in mirror_url

--- spec/classes/mirror_spec.rb
+++ spec/classes/mirror_spec.rb
@@ -12,7 +12,7 @@ describe 'kafka::mirror', type: :class do
     }
   end
 
-  let :params do
+  let :common_params do
     {
       consumer_config: {
         'group.id'          => 'kafka-mirror',
@@ -22,6 +22,10 @@ describe 'kafka::mirror', type: :class do
         'bootstrap.servers' => 'localhost:9092'
       }
     }
+  end
+
+  let :params do
+    common_params
   end
 
   it { is_expected.to contain_class('kafka::mirror::install').that_comes_before('Class[kafka::mirror::config]') }
@@ -93,16 +97,7 @@ describe 'kafka::mirror', type: :class do
 
       context 'service_requires_zookeeper disabled' do
         let :params do
-          {
-            service_requires_zookeeper: false,
-            consumer_config: {
-              'group.id'          => 'kafka-mirror',
-              'zookeeper.connect' => 'localhost:2181'
-            },
-            producer_config: {
-              'bootstrap.servers' => 'localhost:9092'
-            }
-          }
+          common_params.merge(service_requires_zookeeper: false)
         end
 
         it { should_not contain_file('kafka-mirror.service').with_content %r{^Requires=zookeeper.service$} }
@@ -110,19 +105,78 @@ describe 'kafka::mirror', type: :class do
 
       context 'service_requires_zookeeper enabled' do
         let :params do
-          {
-            service_requires_zookeeper: true,
-            consumer_config: {
-              'group.id'          => 'kafka-mirror',
-              'zookeeper.connect' => 'localhost:2181'
-            },
-            producer_config: {
-              'bootstrap.servers' => 'localhost:9092'
-            }
-          }
+          common_params.merge(service_requires_zookeeper: true)
         end
 
         it { should contain_file('kafka-mirror.service').with_content %r{^Requires=zookeeper.service$} }
+      end
+    end
+  end
+
+  context 'with mirror_url' do
+
+    domain_names = {
+      'foobar.com'  => true,
+      'foo.bar.com' => true,
+      '999.bar.com' => true,
+      'foo-bar.com' => true,
+      'no-tld'      => false,
+      'foo.longtld' => false,
+    }
+
+    prefixes = {
+      'http://'   => true,
+      'https://'  => true,
+      'random://' => false,
+    }
+
+    paths = {
+      ''                      => true,
+      '/'                     => true,
+      '/package'              => true,
+      '/package/'             => true,
+      '/another/package'      => true,
+      '/yet/another/package/' => true,
+    }
+
+    ports = {
+      ''        => true,
+      ':9'      => false,
+      ':10'     => true,
+      ':99999'  => true,
+      ':100000' => false,
+    }
+
+    prefixes.each do |prefix, valid_prefix|
+      context "with prefix <#{prefix}>" do
+
+        domain_names.each do |domain_name, valid_domain|
+          context "with domain name <#{domain_name}>" do
+
+            paths.each do |path, valid_path|
+              context "with path <#{path}>" do
+
+                ports.each do |port, valid_port|
+                  context "with port <#{port}>" do
+
+                    mirror_url = "#{prefix}#{domain_name}#{port}#{path}"
+                    context "URL => <#{mirror_url}>" do
+                      let :params do
+                        common_params.merge(mirror_url: mirror_url)
+                      end
+
+                      if valid_domain && valid_prefix && valid_path && valid_port
+                        it { is_expected.to compile }
+                      else
+                        it { expect { is_expected.to compile }.to raise_error(/#{mirror_url} is not a valid url/) }
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
