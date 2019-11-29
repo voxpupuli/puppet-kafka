dff04f281d53339ae6b682243086d102919cacd2
Dennis Hoppe <github@debian-solutions.de>
2019-11-29 15:13:19 +0100

    Clean up spec_helper_acceptance.rb and move Hiera data to spec/fixtures/

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,21 +4,18 @@ require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
 run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
+  # Readable test descriptions
   c.formatter = :documentation
 
+  # Configure all nodes in nodeset
   c.before :suite do
-    hosts.each do |host|
-      if host[:platform] =~ %r{el-7-x86_64} && host[:hypervisor] =~ %r{docker}
-        on(host, "sed -i '/nodocs/d' /etc/yum.conf")
-      end
+    install_module
+    install_module_dependencies
 
-      write_hiera_config_on(host, ['%<::osfamily>s'])
-      copy_hiera_data_to(host, './spec/acceptance/hieradata/')
+    hosts.each do |host|
+      c.host = host
     end
   end
 end
